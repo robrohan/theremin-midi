478f5c06dbf896b410a7172fed819914e6da6fab
Rob Rohan <rob@therohans.com>
2024-06-02 11:26:45 +1200

    Removing fluidsynth

--- src/train.py
+++ src/train.py
@@ -1,11 +1,8 @@
-import collections
-import fluidsynth
 import logging
 import glob
 import numpy as np
 import pathlib
 import pandas as pd
-import pretty_midi
 import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
