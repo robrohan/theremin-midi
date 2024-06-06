b669bff78b4cf2851a8692756c4f0814add4911a
Rob Rohan <rob@therohans.com>
2024-06-06 18:50:28 +1200

    Updating batch sizes and what not

--- src/train.py
+++ src/train.py
@@ -46,10 +46,10 @@ def create_training_dataset(filenames, start=0, num_files=128):
 
 
 def train(filenames,
-          epochs=5,
+          epochs=2,
           batch_size=64,
           seq_length=25,
-          learning_rate=0.03,
+          learning_rate=0.005,
           vocab_size=128):
 
     model = m.create_model(seq_length, learning_rate)
@@ -57,13 +57,12 @@ def train(filenames,
     with open('./output/model.json', 'w') as f:
         f.write(model.to_json())
 
-    batch = 100
+    batch = 50
     start = 0
     end = batch
     for _ in range(math.ceil(len(filenames) / batch)):
         logging.debug(f"start={start} num_files={end} {len(filenames)}")
 
-        # logging.debug(f"-------> {len(filenames[start:end])}")
         notes_ds, notes_n = create_training_dataset(
             filenames, start=start, num_files=end)
         seq_ds = m.create_sequences(notes_ds, seq_length, vocab_size)
@@ -106,65 +105,6 @@ def train(filenames,
         start += batch
         end += batch
 
-    # notes_ds, notes_n = create_training_dataset(
-    #     filenames, start=0, num_files=500)
-    # seq_ds = m.create_sequences(notes_ds, seq_length, vocab_size)
-    # # seq_ds.element_spec
-
-    # # test_ds, test_n = create_training_dataset(filenames, 128, 256)
-    # # test_seq = create_sequences(notes_ds, seq_length, vocab_size)
-
-    # ###############
-
-    # for seq, target in seq_ds.take(1):
-    #     print('sequence shape:', seq.shape)
-    #     print('sequence elements (first 10):', seq[0: 10])
-    #     print()
-    #     print('target:', target)
-
-    # ###############
-
-    # # Batch the examples, and configure the dataset for performance.
-    # buffer_size = notes_n - seq_length  # the number of items in the dataset
-    # train_ds = (seq_ds
-    #             .shuffle(buffer_size)
-    #             .batch(batch_size, drop_remainder=True)
-    #             .cache()
-    #             .prefetch(tf.data.experimental.AUTOTUNE))
-    # # train_ds.element_spec
-
-    # ################
-
-    # # model = m.create_gpt2_model(seq_length, learning_rate)
-
-    # losses = model.evaluate(train_ds, return_dict=True)
-    # print(losses)
-
-    # # model.evaluate(train_ds, return_dict=True)
-
-    # callbacks = [
-    #     tf.keras.callbacks.ModelCheckpoint(
-    #         filepath='./training_checkpoints/ckpt_{epoch}',
-    #         save_weights_only=True),
-    #     tf.keras.callbacks.EarlyStopping(
-    #         monitor='loss',
-    #         patience=5,
-    #         verbose=1,
-    #         restore_best_weights=True),
-    # ]
-    # history = model.fit(
-    #     train_ds,
-    #     epochs=epochs,
-    #     callbacks=callbacks,
-    #     batch_size=batch_size
-    # )
-
-    # plt.plot(history.epoch, history.history['loss'], label='total loss')
-    # plt.savefig('./output/loss.png')
-    # # plt.plot(history.epoch, history.history['accuracy'], label='accuracy')
-    # # plt.savefig('accuracy.png')
-    # # plt.show()
-
     return model
 
 
@@ -175,7 +115,7 @@ def main():
     data_dir = pathlib.Path('robbie-v1.0.0/clean/clean')
     filenames = glob.glob(str(data_dir/'*.mid*'))
     print('Number of files:', len(filenames))
-    train(filenames, seq_length=64)
+    train(filenames)
 
 
 if __name__ == "__main__":
